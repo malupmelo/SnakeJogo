03577940e4d868f031c1186352c479dbd07082e7
Bia Astle <astlebia@gmail.com>
2025-05-05 20:09:46 -0300

    feat: função para mudar a cor da tela

--- src/main.c
+++ src/main.c
@@ -3,3 +3,14 @@
 #include "keyboard.h"
 #include "timer.h"
 
+void cor_tela(screenColor cor){
+    screenSetColor(WHITE,cor);
+    for (int x = 1; x <= 80; x++){
+        for (int y = 1; y<= 24; y++){
+            screenGotoxy(x,y);
+            printf(" ");
+        }
+    }
+    screenUpdate();
+}
+
