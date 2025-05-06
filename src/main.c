fc628d45c55ec510fdd8a0c1c3b2092adecec5ae
Bia Astle <astlebia@gmail.com>
2025-05-06 13:38:27 -0300

    fix: todos as posicoes viravam a cabeca da cobra

--- src/main.c
+++ src/main.c
@@ -24,58 +24,65 @@ void limpar(Posicao *cobra, int tamanho){
     screenSetColor(WHITE,BLACK);
     screenGotoxy(cobra[tamanho-1].x, cobra[tamanho-1].y);
     printf(" ");
+    screenUpdate();
 }
 
 void desenhar_cobra(Posicao *cobra, int tamanho){
     screenSetColor(BLUE, BLACK);
     for (int i = 0; i < tamanho; i++ ){
-        screenGotoxy(cobra[tamanho].x, cobra[tamanho].y);
+        screenGotoxy(cobra[i].x, cobra[i].y);
         printf("@");
     }
     screenUpdate();
 }
 
 void mover_cobra(Posicao *cobra, int tamanho, int tecla){
-    for (int i = 0; i < tamanho; i++){
-        cobra[i+1] = cobra[i];
+    for (int i = tamanho-1; i > 0; i--){
+        cobra[i] = cobra[i-1];
     }
     if (tecla == 119){
-        limpar(cobra,tamanho);
         cobra[0].y--;
     }
     else if (tecla == 115){
-        limpar(cobra,tamanho);
         cobra[0].y++;
     }
     else if (tecla == 97){
-        limpar(cobra,tamanho);
         cobra[0].x--;
     }
     else if(tecla == 100){
-        limpar(cobra,tamanho);
         cobra[0].x++;
     }
-    desenhar_cobra(cobra, tamanho);
 }
 
 
 int main(){
     screenInit(0);
     keyboardInit();
+    timerInit(200);
     cor_tela(BLACK);
 
     int tamanho = 5;
     int tecla = 0;
     Posicao *cobra = malloc(tamanho * sizeof(Posicao));
-    cobra[0].x = 10;
-    cobra[0].y = 10;
+    for (int i = 0; i<tamanho;i++){
+        cobra[i].x = 10-i;
+        cobra[i].y = 10;
+    }
+    desenhar_cobra(cobra,tamanho);
 
     while (tecla != 10){
         if (keyhit()){
             tecla = readch();
         }
-        mover_cobra(cobra, tamanho, tecla);
+        if(timerTimeOver()){
+            limpar(cobra,tamanho);
+            mover_cobra(cobra,tamanho,tecla);
+            desenhar_cobra(cobra,tamanho);
+        }
+        
     }
+    free(cobra);
+    timerDestroy();
     keyboardDestroy();
     screenDestroy();
 
