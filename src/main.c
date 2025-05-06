26f35b3c4cc986e23ac379aeb286b2b585fba7ec
Bia Astle <astlebia@gmail.com>
2025-05-06 09:07:45 -0300

    feat: Funções para desenhar cobrinha e limpar última posição

--- src/main.c
+++ src/main.c
@@ -14,3 +14,35 @@ void cor_tela(screenColor cor){
     screenUpdate();
 }
 
+struct Posicao{
+    int x;
+    int y;
+} typedef Posicao;
+
+void limpar(Posicao *cobra, int tamanho){
+    screenSetColor(WHITE,BLACK);
+    screenGotoxy(cobra[tamanho-1].x, cobra[tamanho-1].y);
+    printf(" ");
+    screenupdate();
+}
+
+void desenhar_cobra(Posicao *cobra, int tamanho){
+    screenSetColor(BLUE, BLACK);
+    for (int i = 0; i < tamanho; i++ ){
+        screenGotoxy(cobra[tamanho].x, cobra[tamanho].y);
+        printf("@");
+    }
+    screenUpdate();
+}
+
+
+int main(){
+    screenInit(0);
+    keyboardInit();
+    cor_tela(BLACK);
+
+    int tamanho = 1;
+
+    return 0;
+}
+
